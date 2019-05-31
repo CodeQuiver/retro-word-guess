55fd2f6c9b0c26a56f5b88f5b43d7e68e07d8a38
CodeQuiver <38484031+CodeQuiver@users.noreply.github.com>
2019-05-31 17:41:39 -0400

    pseudo-coding main javascript file using my separate written program flow diagram

---
+++ assets/javascript/game.js
@@ -0,0 +1,56 @@
+// import utility functions
+
+
+// ------- INITIALIZE Global Variables --------//
+
+    // WORD List Object: will include several arrays of words with different video-game or retro themes
+    // for computer to randomly pick from- Category names can then be used as hints if time to add that feature
+        // Category 1: [array of words]
+        // Category 2: [array of words]
+        // Category 3: [array of words]
+        // Category 4: [array of words]
+        // Category 5: [array of words]
+    // END WORD List Object
+
+    // Computer's Answer
+    let compAnswer = "Sorry, I need to pick a word!";
+
+    // User's Letter Guess
+    let userLetter = "";
+
+
+// ------- END INITIALIZE Global Variables --------//
+
+
+// ------- FUNCTION Definitions -------- //
+
+    // RESET GAME FUNCTION - Resets game entirely, including returning all scores to 0 //
+    // END RESET GAME FUNCTION //
+
+    // NEW ROUND FUNCTION - Starts a new round but retains the win/loss count //
+    // END NEW ROUND FUNCTION //
+
+    // COMPUTER SELECT ANSWER FUNCTION //
+    // END COMPUTER SELECT ANSWER FUNCTION //
+
+    // CHECK LETTER GUESS FUNCTION - compares user's letter to the characters in compAnswer //
+    // END CHECK LETTER GUESS FUNCTION //
+
+    // WIN CHECK FUNCTION //
+    // END WIN CHECK FUNCTION //
+
+
+
+// ------- END FUNCTION Definitions -------- //
+
+// ------- MAIN PROGRAM FLOW ------- //
+
+
+
+
+
+
+
+
+
+// ------- END MAIN PROGRAM FLOW ------- //
